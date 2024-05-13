18855c8c30b22d380c00607da133ef71edd2acbf
ve4nick <ve4nick98@gmail.com>
2024-05-13 17:31:43 +0300

    Create views for Position (List, Create, Update, Delete)

--- board/views.py
+++ board/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from board.models import Worker, Position
+
+
+class PositionListView(generic.ListView):
+    model = Position
+    context_object_name = "position_list"
+    template_name = "board/position_list.html"
+
+
+class PositionCreateView(generic.CreateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("board:position-list")
+
+
+class PositionUpdateView(generic.UpdateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("board:position-list")
+
+
+class PositionDeleteView(generic.DeleteView):
+    model = Position
+    success_url = reverse_lazy("board:position-list")
+
