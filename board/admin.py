a6657d2ee0b5f590a89e59c2e587cda29947c87f
ve4nick <ve4nick98@gmail.com>
2024-08-07 20:25:46 +0300

    Register models in admin.py

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Position, Worker, TaskType, Task
 
-# Register your models here.
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "position")
+    search_fields = ("username", "email", "position__name")
+    list_filter = ("position",)
+
+
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type")
+    search_fields = ("name", "description", "task_type__name")
+    list_filter = ("is_completed", "priority", "task_type")
+    filter_horizontal = ("assignees",)
+
+
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Worker, WorkerAdmin)
+admin.site.register(TaskType, TaskTypeAdmin)
+admin.site.register(Task, TaskAdmin)
