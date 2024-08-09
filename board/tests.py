42d684c58a59318e5da170cca020bd6f9b2bb4a0
ve4nick <ve4nick98@gmail.com>
2024-08-09 17:07:59 +0300

    Write tests for models overrided methods

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from board.models import (
+    Position,
+    Worker,
+    TaskType,
+    Task,
+)
+
+
+class ModelsTest(TestCase):
+    def setUp(self):
+        self.position = Position.objects.create(name="testPosition")
+        self.worker = Worker.objects.create(username="testWorker", position=self.position)
+        self.task_type = TaskType.objects.create(name="testTaskType")
+        self.task = Task.objects.create(name="testTask", description="test", deadline="2026-08-31 18:53:00", is_completed=False, priority="urgent", task_type=self.task_type)
+        self.task.assignees.set([self.worker])
+
+    def test_position_str(self):
+        self.assertEqual(str(self.position), self.position.name)
+
+    def test_worker_get_absolute_url(self):
+        expected_url = reverse("board:worker-detail", kwargs={"pk": self.worker.pk})
+        actual_url = self.worker.get_absolute_url()
+        self.assertEqual(actual_url, expected_url)
+
+    def test_task_type_str(self):
+        self.assertEqual(str(self.task_type), self.task_type.name)
+
+    def test_task_type_get_absolute_url(self):
+        expected_url = reverse("board:task-detail", kwargs={"pk": self.task.pk})
+        actual_url = self.task.get_absolute_url()
+        self.assertEqual(actual_url, expected_url)
