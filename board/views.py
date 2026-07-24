c1997e2ac9bbfa286af6b12dd32036da695d771d
ve4nick <ve4nick98@gmail.com>
2026-07-24 14:56:17 +0300

    Fix: pass form class instance instead of form class

--- board/views.py
+++ board/views.py
@@ -59,7 +59,7 @@ class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
 class TaskTypeListCreateView(LoginRequiredMixin, View):
     def get(self, request):
         task_types = TaskType.objects.all()
-        form = TaskTypeForm
+        form = TaskTypeForm()
         return render(request, 'board/tasktype_list.html', {'form': form, 'task_type_list': task_types})
 
     def post(self, request):
